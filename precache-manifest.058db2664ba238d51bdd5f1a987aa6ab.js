592e9a3b27fd2a79e951d349fd966dbccec35889
Trung Nguyen <atom.911@hotmail.com>
2019-10-24 14:43:41 +0700

    Updates

---
+++ precache-manifest.058db2664ba238d51bdd5f1a987aa6ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e1b946c0f53302517e24106f20ea8c",
+    "url": "/itservice/index.html"
+  },
+  {
+    "revision": "679c6684f18adb5e62eb",
+    "url": "/itservice/static/css/main.60747779.chunk.css"
+  },
+  {
+    "revision": "2e420f6ca9cf3da4baa0",
+    "url": "/itservice/static/js/2.220293a8.chunk.js"
+  },
+  {
+    "revision": "679c6684f18adb5e62eb",
+    "url": "/itservice/static/js/main.92699a28.chunk.js"
+  },
+  {
+    "revision": "c419e66349772222c69f",
+    "url": "/itservice/static/js/runtime~main.d89582fc.js"
+  },
+  {
+    "revision": "141b7e522b712a939e873717895ce43c",
+    "url": "/itservice/static/media/banner.141b7e52.jpg"
+  },
+  {
+    "revision": "cea12eb7442c87ff77431eb778b5add9",
+    "url": "/itservice/static/media/home_itservice_slide1-890x440.cea12eb7.png"
+  },
+  {
+    "revision": "c6f7fbaddcc1ad1d4a60ad943b2c5a04",
+    "url": "/itservice/static/media/home_itservice_slide2-890x440.c6f7fbad.png"
+  }
+]);
